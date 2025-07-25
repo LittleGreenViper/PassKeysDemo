e165c5d052225819809e40655cddd79776edbdf6
Chris Marshall <chris@riftvalleysoftware.com>
2025-07-25 10:02:20 -0400

    Added the users table to the MySQL structure. Renamed the UIKt app.

--- Server/MySQLStructure.sql
+++ Server/MySQLStructure.sql
@@ -1,7 +1,13 @@
 DROP TABLE IF EXISTS `webauthn_credentials`;
 CREATE TABLE `webauthn_credentials` (
-  `id` int(11) NOT NULL,
   `user_id` varchar(255) NOT NULL,
   `credential_id` varbinary(255) NOT NULL,
   `public_key` text NOT NULL
+);
+DROP TABLE IF EXISTS `passkeys_demo_users`;
+CREATE TABLE `passkeys_demo_users` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_credo` text
 );
