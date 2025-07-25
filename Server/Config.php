f1af107ec10096d79d0fa960a23da0fe3e4c1f26
Chris Marshall <chris@riftvalleysoftware.com>
2025-07-25 11:02:55 -0400

    Code commenting and basic structure stuff.

--- Server/Config.php
+++ Server/Config.php
@@ -1,18 +1,40 @@
 <?php
+/***********************/
+/**
+This is a static class that we use to provide our installation-depended configuration details.
+ */
 class Config {
+    /// This is the readable name to be used to describe the organization hosting the server.
     static $g_relying_party_name = "<NAME OF ORG>";
+    /// This is the internal database name.
     static $g_db_name = '<DATABASE NAME>';
+    /// This is the internal host DNS name or IP address (usually localhost).
     static $g_db_host = 'localhost';
+    /// This is the database type (currently, we are specifying MySQL only).
     static $g_db_type = 'mysql';
+    /// This is the database username (Must have all satandard permissions).
     static $g_db_login = '<DB LOGIN>';
+    /// This is the database user password.
     static $g_db_password = '<DB PASSWORD>';
 }
-    
-function base64url_encode($data) {
+
+// MARK: - Global Utility Functions
+
+/***********************/
+/**
+\returns the data provided, as a Base64URL-encoded string.
+ */
+function base64url_encode(  $data   ///< The data (can be binary) to be converted to Base64URL
+                        ) {
     return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
 }
 
-function base64url_decode(string $data): string|false {
+/***********************/
+/**
+\returns the Base64URL-encoded string provided, as the original (possibly binary) data. FALSE, if the conversion fails.
+ */
+function base64url_decode(string $data  ///< The Base64URL-encoded string to be converted to its original data.
+                        ): string|false {
     $data = strtr($data, '-_', '+/');
     $padding = strlen($data) % 4;
     if ($padding > 0) {
