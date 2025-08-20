5c9bc0b8fbd7f3c3925080af78c25cc372247957
Chris Marshall <chris@riftvalleysoftware.com>
2025-08-20 08:42:59 -0400

    Moved a couple of files into a subfolder. Made all the quotes single.

--- Server/Config.php
+++ Server/Config.php
@@ -5,9 +5,9 @@ This is a static class that we use to provide our installation-depended configur
  */
 class Config {
     /// This is the readable name to be used to describe the organization hosting the server.
-    static $g_relying_party_name = "<NAME OF ORG>";
+    static $g_relying_party_name = '<NAME OF ORG>';
     /// This is the URI to be used to denote the organization hosting the server.
-    static $g_relying_party_uri = "<URI OF SERVER>";
+    static $g_relying_party_uri = '<URI OF SERVER>';
     /// This is the internal database name.
     static $g_db_name = '<DATABASE NAME>';
     /// This is the internal host DNS name or IP address (usually localhost).
